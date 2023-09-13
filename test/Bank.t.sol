c8a8b4019ed4cb4d6741917c4cd6e55121b4cb0e
dawid.d <dawid.d@dawid.d.com>
2023-09-13 10:31:58 +0200

    Add base test files

---
+++ test/Bank.t.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console2.sol";
+
+import "./BaseTest.sol";
+
+contract BankTest is BaseTest {
+    
+    function test1() public {
+    }
+}
---
+++ test/BaseTest.sol
@@ -0,0 +1,34 @@
+pragma solidity 0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console2.sol";
+import "solmate/test/utils/mocks/MockERC20.sol";
+
+abstract contract BaseTest is Test {
+    uint256 constant USDC_1 = 1e6;
+    uint256 constant USDC_100K = 1e11; // 1e5 = 100K tokens with 6 decimals
+    uint256 constant TOKEN_1 = 1e18;
+    uint256 constant TOKEN_100K = 1e23; // 1e5 = 100K tokens with 18 decimals
+
+    MockERC20 USDC;
+    MockERC20 DAI;
+
+    function deployOwners() public {
+        owners = new address[](3);
+        owners[0] = address(0x1);
+        owners[1] = address(0x2);
+        owners[2] = address(0x3);
+    }
+
+    function deployCoins() public {
+        USDC = new MockERC20("USDC", "USDC", 6);
+        DAI = new MockERC20("DAI", "DAI", 18);
+    }
+
+    function mintStables() public {
+        for (uint256 i = 0; i < owners.length; i++) {
+            USDC.mint(owners[i], 1e12 * USDC_1);
+            DAI.mint(owners[i], 1e12 * TOKEN_1);
+        }
+    }
+}
