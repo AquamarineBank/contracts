0fb7e4e07777ce54fbabdecd8c03fa97a163b8f6
Ceazor <73511897+Ceazor@users.noreply.github.com>
2023-09-15 13:35:31 -0600

    assertFail on boardroom.testStakeAquaEarnClaim()

--- test/Boardroom.t.sol
+++ test/Boardroom.t.sol
@@ -4,6 +4,7 @@ import "forge-std/Test.sol";
 import "forge-std/console2.sol";
 
 import "./BaseTest.sol";
+import "./Bank.t.sol";
 import "contracts/Bank.sol";
 import "contracts/USD.sol";
 import "contracts/AQUA.sol";
@@ -14,6 +15,7 @@ contract BoardroomTest is BaseTest {
     OneUSD oneUSDContract;
     Boardroom boardroomContract;
     Aquamarine aqua;
+    BankTest banktest;
 
     function setUp() public {
         deployOwners();
@@ -69,6 +71,47 @@ contract BoardroomTest is BaseTest {
         assertEq(depositBalAfterWithdraw, 0);        
     }
 
+    function testStakeAquaEarnClaim() public {        
+        aqua.approve(address(boardroomContract), TOKEN_1);
+        boardroomContract.deposit(TOKEN_1);
+
+        testMint18DeciThenRedeem();
+
+        uint256 rewardsSentToBoardroom = oneUSDContract.balanceOf(address(boardroomContract));
+
+        vm.warp(604800);
+
+        address[] memory rewards = new address[](1);
+        rewards[0] = address(oneUSDContract);
+        boardroomContract.getReward(address(owners[0]), rewards);
+
+        uint256 oneUSDBalAfterGetReward = oneUSDContract.balanceOf(address(owners[0]));
+        
+        assertEq(oneUSDBalAfterGetReward, rewardsSentToBoardroom);
+    }
+
+    function testMint18DeciThenRedeem() public {
+        DAI.approve(address(bankContract), TOKEN_1);
+        bankContract.deposit(address(DAI), TOKEN_1);
+        
+        uint256 oneUsdBalanceBefore = oneUSDContract.balanceOf(address(owners[0]));
+        uint256 oneUsdBalanceBeforeRewards = oneUSDContract.balanceOf(address(boardroomContract));
+        uint256 DAIBalanceBefore = DAI.balanceOf(address(owners[0]));
+        uint256 DAIBalanceBankBefore = DAI.balanceOf(address(bankContract));
+
+        oneUSDContract.approve(address(bankContract), oneUsdBalanceBefore);
+        bankContract.redeem(address(DAI), oneUsdBalanceBefore);
+
+        uint256 DAIBalanceAfter = DAI.balanceOf(address(owners[0]));
+        uint256 oneUsdBalancAfter = oneUSDContract.balanceOf(address(owners[0]));
+        uint256 oneUsdBalanceAfterRewards = oneUSDContract.balanceOf(address(boardroomContract));
+        uint256 DAIBalanceBankAfter = DAI.balanceOf(address(bankContract));
+
+        assertEq(oneUsdBalanceBefore - oneUsdBalancAfter,TOKEN_1);
+        assertEq(DAIBalanceAfter - DAIBalanceBefore,TOKEN_1 * bankContract.redeemFee() / 1000);
+        assertEq(DAIBalanceBankBefore - DAIBalanceBankAfter,TOKEN_1 * bankContract.redeemFee() / 1000);
+        assertEq(oneUsdBalanceAfterRewards - oneUsdBalanceBeforeRewards,TOKEN_1 - (TOKEN_1 * bankContract.redeemFee() / 1000));
+    }
 
     
 }
